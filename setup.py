3fb9c8d476d9cab039024d989490b89ca8634879
Kishan Bagaria <KishanBagaria@users.noreply.github.com>
2019-09-23 00:43:01 +0530

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
   name='dAbot',
-  version='2019.07.18',
+  version='2019.09.22',
   description='CLI tool to automate stuff on DeviantArt.com',
   url='https://github.com/KishanBagaria/dAbot',
   author='Kishan Bagaria',
